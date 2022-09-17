8a889039f08f44f65893be1457c6fd871dadad23
thiago <thiagoedusan5.11@outlook.com>
2022-09-17 13:18:11 -0300

    feat: foi feita a rota get/cars

--- src/router/routeCars.ts
+++ src/router/routeCars.ts
@@ -1,9 +1,7 @@
 import { Router } from 'express';
-
 import CarController from '../controllers/CarController';
 
 const route = Router();
-
 const controller: CarController = new CarController();
 
 route.post('/', controller.create);
