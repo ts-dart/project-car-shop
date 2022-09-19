c3311f75e5c6e2d4f7dfaec9e3c1a6fc1ce6e399
thiago <thiagoedusan5.11@outlook.com>
2022-09-19 19:50:43 -0300

    test: testes unitarios realizados nos metodos da classe da camada controller

--- src/tests/unit/controllers/carController.test.ts
+++ src/tests/unit/controllers/carController.test.ts
@@ -34,3 +34,103 @@ describe('carController/create', () => {
     expect(res.send.getCall(0).args[0]).to.deep.equals(carMock);
   });
 });
+
+describe('carController/read', () => {
+  const res: any = {};
+  before(async () => {
+    res.status = sinon.stub().returns(res);
+    res.send = sinon.stub().returns(res);
+ });
+
+  after(()=>{
+   sinon.restore();
+  })
+
+  it('envia a req para o service e responde o usuario', async () => {
+    const fakeCarService = {
+      read() {
+        return [carMock];
+      }
+    }
+
+    const controller = new CarController(fakeCarService as any);
+    await controller.read({} as Request, res as Response);
+    expect(res.status.getCall(0).args[0]).to.equal(200);
+    expect(res.send.getCall(0).args[0]).to.deep.equals([carMock]);
+  });
+});
+
+describe('carController/readOne', () => {
+  const res: any = {};
+  before(async () => {
+    res.status = sinon.stub().returns(res);
+    res.send = sinon.stub().returns(res);
+ });
+
+  after(()=>{
+   sinon.restore();
+  })
+
+  it('envia a req para o service e responde o usuario', async () => {
+    const fakeCarService = {
+      readOne() {
+        return carMock;
+      }
+    }
+
+    const controller = new CarController(fakeCarService as any);
+    await controller.readOne({} as Request, res as Response);
+    expect(res.status.getCall(0).args[0]).to.equal(200);
+    expect(res.send.getCall(0).args[0]).to.deep.equals(carMock);
+  });
+});
+
+describe('carController/update', () => {
+  const res: any = {};
+  before(async () => {
+    res.status = sinon.stub().returns(res);
+    res.send = sinon.stub().returns(res);
+ });
+
+  after(()=>{
+   sinon.restore();
+  })
+
+  it('envia a req para o service e responde o usuario', async () => {
+    const fakeCarService = {
+      update() {
+        return carMock;
+      }
+    }
+
+    const controller = new CarController(fakeCarService as any);
+    await controller.update({} as Request, res as Response);
+    expect(res.status.getCall(0).args[0]).to.equal(200);
+    expect(res.send.getCall(0).args[0]).to.deep.equals(carMock);
+  });
+});
+
+describe('carController/delete', () => {
+  const res: any = {};
+  before(async () => {
+    res.status = sinon.stub().returns(res);
+    res.send = sinon.stub().returns(res);
+ });
+
+  after(()=>{
+   sinon.restore();
+  })
+
+  it('envia a req para o service e responde o usuario', async () => {
+    const fakeCarService = {
+      delete() {
+        return {};
+      }
+    }
+
+    const controller = new CarController(fakeCarService as any);
+    await controller.delete({} as Request, res as Response);
+    expect(res.status.getCall(0).args[0]).to.equal(200);
+    expect(typeof res.send.getCall(0).args[0]).to.deep.equals('object');
+  });
+});
